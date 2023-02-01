a2351001017ec9a04da6c9a525364e48b2b894d9
David Torres <dftp93@gmail.com>
2023-02-01 17:41:19 +0000

    Add content for locomotive scroll smooth

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Intento de codespace, trabajo remoto
 
 # Fuente de inspiracion
 https://justusbremer.com/
+
+# Smoot Scroll
+https://locomotivemtl.github.io/locomotive-scroll/
+Investigte this content for a smooth scroll in the page
--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log("123 probando")
+import LocomotiveScroll from '../node_modules/locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+
+alert("Hola Mundo")
